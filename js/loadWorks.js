22857ddc1b4289f4b8046e5ef9d032b64573d02e
p1nh0 <p1nh0.c0d1ng@gmail.com>
2025-08-10 14:29:53 +0100

    now loads works from external links on a separate tab

--- js/loadWorks.js
+++ js/loadWorks.js
@@ -11,14 +11,19 @@ fetch('/works/index.json')
       const preview = document.createElement('div');
       preview.className = 'work-preview-container';
 
+      // Detect if it's an external link
+      const isExternal = /^https?:\/\//i.test(work.file);
+      const link = isExternal ? work.file : `/works/${work.file}`;
+      const targetAttr = isExternal ? ' target="_blank" rel="noopener noreferrer"' : '';
+
       preview.innerHTML = `
-        <a href="/works/${work.file}">
+        <a href="${link}"${targetAttr}>
           <img src="${work.thumbnail}" alt="${work.title}" class="work-thumbnail" />
         </a>
         <div class="work-details">
           <div class="work-title">${work.title}</div>
           <div class="work-description">${work.description}</div>
-          <a href="/works/${work.file}">Read more</a>
+          <a href="${link}"${targetAttr}s>Read more</a>
         </div>
       `;
 
